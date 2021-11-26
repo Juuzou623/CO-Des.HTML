764339f3492fb91ada6c9d59b40d46986969244b
brigidolivia <livia.outreach@gmail.com>
2021-11-26 11:57:07 -0300

    Botão tarefa quase funcionando

--- JavaS/index.js
+++ JavaS/index.js
@@ -1,7 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
         tarefas=document.querySelector('.popup');
         imageMode= document.querySelector('.botao_taf');
-
         imageMode.addEventListener('click', function(event) {
             tarefas.style.visibility = "visible"
         })
