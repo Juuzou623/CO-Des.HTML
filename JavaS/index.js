6aac738464d7878025b1651a0cbd7f05537995e3
brigidolivia <livia.outreach@gmail.com>
2021-12-01 08:20:38 -0300

    .

--- JavaS/index.js
+++ JavaS/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-        tarefas=document.querySelector('.popup');
+        tarefas=document.querySelector('.popup_tarefa');
         imageMode= document.querySelector('.botao_taf');
+        pagina_anterior= document.querySelector('body')
 
         visivel = true
 
