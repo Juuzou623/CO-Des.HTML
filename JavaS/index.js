bbaf07c7fe20a0a5e6f48e71b71abe9899c5e23d
brigidolivia <livia.outreach@gmail.com>
2021-12-07 13:51:05 -0300

    Pop up com fundo

--- JavaS/index.js
+++ JavaS/index.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
         tarefas=document.querySelector('.popup_tarefa');
         imageMode= document.querySelector('.botao_taf');
-        pagina_anterior= document.querySelector('body')
+        const body = document.body;
 
         visivel = true
 
         imageMode.addEventListener('click', function(event) {
             if (visivel==false){
                 tarefas.style.visibility = "visible"
+                body.style.background = "rgba(0,0,0,0.7)";
                 visivel=true
             }
             else{
                 tarefas.style.visibility = "hidden"
+                body.style.background = "rgba(255,255,255,1)";
                 visivel=false
             }
             
