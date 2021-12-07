5f026c236fcc1b3262928fce0eea8abf2928526d
brigidolivia <livia.outreach@gmail.com>
2021-12-07 16:19:03 -0300

    pra ver se a mia vê

--- JavaS/Piso1.js
+++ JavaS/Piso1.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     registrar=document.querySelector('.popup_registro');
-    imageMode_Rs= document.querySelectorAll('.Vaga');
+    imageMode_Rs_vazias= document.querySelectorAll('.Vaga_vazia');
     const body = document.body;
 
     regis_visivel = true
-    imageMode_Rs.forEach (imageMode_R =>{
-    imageMode_R.addEventListener('click', function(event) {
-        
+    imageMode_Rs_vazias.forEach (imageMode_R_v =>{
+    imageMode_R_v.addEventListener('click', function(event) {
             if (regis_visivel==false){
                 registrar.style.visibility = "visible"
                 body.style.background = "rgba(0,0,0,0.7)";
@@ -18,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 body.style.background = "rgba(255,255,255,1)";
                 regis_visivel=false
             }
-    
     })
     })
 });
