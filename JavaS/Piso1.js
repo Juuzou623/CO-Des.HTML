d9fb50e715f23326e13fa706bb2db3b6bfc92719
brigidolivia <livia.outreach@gmail.com>
2021-12-07 21:19:11 -0300

    Quase lá com a legenda

--- JavaS/Piso1.js
+++ JavaS/Piso1.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     registrar=document.querySelector('.popup_registro');
     imageMode_Rs_vazias= document.querySelectorAll('.Vaga_vazia');
+    enviar=document.querySelector('#registrando')
 
     const body = document.body;
 
     regis_visivel = true
 
+    i=-1
     imageMode_Rs_vazias.forEach (imageMode_R_v =>{
     imageMode_R_v.addEventListener('click', function(event) {
-            if (regis_visivel==false){
-                registrar.style.visibility = "visible"
-                body.style.background = "rgba(0,0,0,0.7)";
-                regis_visivel=true
-                pagina_anterior.style.background = "rgba(0,0,0,0.5)";
-            }
-            else{
-                registrar.style.visibility = "hidden"
-                body.style.background = "rgba(255,255,255,1)";
-                regis_visivel=false
-            }
+        if (regis_visivel==false){
+            registrar.style.visibility = "visible"
+            body.style.background = "rgba(0,0,0,0.7)";
+            regis_visivel=true
+            pagina_anterior.style.background = "rgba(0,0,0,0.5)";
+        ;}
+
+        else{
+            registrar.style.visibility = "hidden"
+            body.style.background = "rgba(255,255,255,1)";
+            regis_visivel=false
+        }
     })
-    });
+    })   
 
     retirada = document.querySelector('.popup_retirada');
     imageMode_Rets_triangulo = document.querySelectorAll('.Vaga_triangulo');
@@ -43,5 +46,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
         })
         });
+
+        legendaa = document.querySelector('.popup_legenda');
+        imageMode_leg=document.querySelector('.botao_leg');
+        leg_visivel = true
+        imageMode_leg.addEventListener('click', function(event) {
+
+                if (leg_visivel==false){
+                    legendaa.style.visibility = "visible"
+                    body.style.background = "rgba(0,0,0,0.7)";
+                    leg_visivel=true
+                    pagina_anterior.style.background = "rgba(0,0,0,0.5)";
+                }
+
+                else{
+                    legendaa.style.visibility = "hidden"
+                    body.style.background = "rgba(255,255,255,1)";
+                    leg_visivel=false
+                }
+        });  
 
     });
