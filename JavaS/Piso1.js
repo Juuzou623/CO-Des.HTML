19ab02807bf90419b3e360fb0b054595812df05e
Juuzou623 <pedrohois@al.insper.edu.br>
2021-12-08 00:20:56 -0300

    jj

--- JavaS/Piso1.js
+++ JavaS/Piso1.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     registrar=document.querySelector('.popup_registro');
     imageMode_Rs_vazias= document.querySelectorAll('.Vaga_vazia');
     enviar=document.querySelector('#registrando')
-
-    const body = document.body;
+    body = document.querySelector('body')
 
     regis_visivel = false
     var escuridao = document.createElement("div");
@@ -12,23 +11,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     i=-1
     imageMode_Rs_vazias.forEach (imageMode_R_v =>{
-    imageMode_R_v.addEventListener('click', function(event) {
-        if (regis_visivel==false){
-            registrar.style.visibility = "visible"
-            body.appendChild(escuridao);
-            regis_visivel=true
-        }
-        else{
-            registrar.style.visibility = "hidden"
-            body.removeChild(escuridao);
-            regis_visivel=false
-        }
-    })
-    })   
+        imageMode_R_v.addEventListener('click', function(event) {
+            if (regis_visivel==false){
+                registrar.style.visibility = "visible"
+                body.appendChild(escuridao);
+                regis_visivel=true
+            }
+            else{
+                registrar.style.visibility = "hidden"
+                body.removeChild(escuridao);
+                regis_visivel=false
+            }
+        });
+    });
 
     retirada = document.querySelector('.popup_retirada');
     imageMode_Rets_triangulo = document.querySelectorAll('.Vaga_triangulo');
     ret_visivel = false
+
 
     imageMode_Rets_triangulo.forEach (imageMode_R_t =>{
         imageMode_R_t.addEventListener('click', function(event) {
@@ -45,36 +45,28 @@ document.addEventListener('DOMContentLoaded', function () {
                 body.removeChild(escuridao);
                 ret_visivel=false
             }
-        })
-
-
-
-
-        registrar=document.querySelector('.botao_retirar');
-        registrar.addEventListener('click', function(event) {
-            imageMode_R_t.src = "Imagens/Circulo-vazio.png"
-        })
+        });
     });
 
-        legendaa = document.querySelector('.popup_legenda');
-        imageMode_leg=document.querySelector('.botao_leg');
-        leg_visivel = false
+    legendaa = document.querySelector('.popup_legenda');
+    imageMode_leg=document.querySelector('.botao_leg');
+    leg_visivel = false
 
-        imageMode_leg.addEventListener('click', function(event) {
-            if (leg_visivel==false){
-                legendaa.style.visibility = "visible"
-                leg_visivel=true
-                var escuridao = document.createElement("div");
-                escuridao.id = "escuridao"
-                body.appendChild(escuridao);
-            }
+    imageMode_leg.addEventListener('click', function(event) {
+        if (leg_visivel==false){
+            legendaa.style.visibility = "visible"
+            leg_visivel=true
+            var escuridao = document.createElement("div");
+            escuridao.id = "escuridao"
+            body.appendChild(escuridao);
+        }
 
-            else{
-                legendaa.style.visibility = "hidden"
-                body.removeChild(escuridao);
-                leg_visivel=false
-            }
-    
-        }); 
+        else{
+            legendaa.style.visibility = "hidden"
+            body.removeChild(escuridao);
+            leg_visivel=false
+        }
+
+    }); 
 
 });
